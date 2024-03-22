ba8fbfd9bfca31c1a419b50f920d71899e88743a
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2024-03-22 06:23:11 +0100

    Added twist.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from direct.showbase.ShowBase import ShowBase
 
 from make_models import make_ring
 
-num_rings = 50
 ring_distance = 3.0
 ring_rotation = 10.0
 ship_speed = 5.0
@@ -18,26 +17,30 @@ base.accept('escape', base.task_mgr.stop)
 
 
 class Ring:
-    def __init__(self, spec):
-        tex_name, segments, color, up_angle = spec
+    def __init__(self, rot_spec, spec):
+        up_angle, twist = rot_spec
+        self.up_angle = up_angle
+        self.twist = twist
+        tex_name, segments, color = spec
         # Center of the ring
         self.center = NodePath("center")
-        self.up_angle = up_angle
         if up_angle == 0.0:
-            self.center.set_pos(0, ring_distance, 0)
+            self.center.set_y(ring_distance)
+            self.center.set_r(twist)
         else:
             circumference = 360.0 / up_angle * ring_distance
             radius = circumference / (2.0 * math.pi)
             self.radius = radius
-            self.center.set_pos(0, 0, radius)
-            self.center.set_hpr(0, up_angle, 0)
+            self.center.set_hpr(0, 0, twist)
+            self.center.set_pos(self.center, 0, 0, radius)
+            self.center.set_hpr(self.center, 0, up_angle, 0)
             self.center.set_pos(self.center, 0, 0, -radius)
         # The ring itself, which is just decorative
         petals = base.loader.load_texture(tex_name)
         ring = make_ring(segments)
         ring.set_texture(petals)
         ring.set_transparency(TransparencyAttrib.M_binary)
-        ring.set_color(*color)
+        #ring.set_color(*color)
         ring.reparent_to(self.center)
         self.ring = ring
         self.rotation_direction = random.random() ** 2
@@ -56,12 +59,12 @@ class Ring:
 
 
 class Level:
-    def __init__(self, num_rings, tunnel_spec):
+    def __init__(self, course_specs, ring_specs):
         # Setting up the rings
         self.rings = []
-        ring_specs = itertools.cycle(tunnel_spec)
-        for _ in range(num_rings):
-            self.rings.append(Ring(next(ring_specs)))
+        full_specs = zip(course_specs, itertools.cycle(ring_specs))
+        for rot_spec, ring_spec in full_specs:
+            self.rings.append(Ring(rot_spec, ring_spec))
         for idx, ring in enumerate(self.rings):
             if idx % 2 == 0:
                 ring.rotation_direction *= -1
@@ -96,40 +99,51 @@ class Level:
         # distance?
         local_advance = self.advance % ring_distance
         local_advance_ratio = local_advance / ring_distance
-        # Now we do the same math that we did to place the ring.
+        anti_ratio = 1.0 - local_advance_ratio
+        # Now we do the same math that we did to place the ring, but in
+        # reverse.
         ring = self.rings[self.current_ring]
         if ring.up_angle == 0.0:
+            base.cam.set_pos(0, 0, 0)
             base.cam.set_hpr(0, 0, 0)
-            base.cam.set_pos(0, -ring_distance * (1 - local_advance_ratio), 0)
+
+            base.cam.set_r(-ring.twist * anti_ratio)
+            base.cam.set_y(-ring_distance * anti_ratio)
         else:
-            base.cam.set_pos(0, 0, ring.radius)
-            base.cam.set_hpr(0, -ring.up_angle * (1 - local_advance_ratio), 0)
-            base.cam.set_pos(base.cam, 0, 0, -ring.radius)
-            print(local_advance_ratio)
+            base.cam.set_pos(0, 0, 0)
+            base.cam.set_hpr(0, 0, 0)
+
+            base.cam.set_z(base.cam, ring.radius)
+            base.cam.set_p(base.cam, -ring.up_angle * anti_ratio)
+            base.cam.set_z(base.cam, -ring.radius)
+            base.cam.set_r(base.cam, -ring.twist * anti_ratio)
 
         # Rotate rings
         for idx in range(len(self.rings)):
             ring = self.rings[idx]
-            ring.rotate(360.0 / ring_rotation * globalClock.dt)
+            ring.rotate(ring_rotation * globalClock.dt)
         # Dooone!
         return task.cont
 
 
 ring_specs = [
-    ("petal_1.png", 9, (1.0, 0.3, 0.3, 1), 3.0),
-    ("petal_2.png", 8, (0.1, 0.8, 0.1, 1), 3.0),
-    ("petal_3.png", 5, (0.8, 0.0, 0.8, 1), 3.0),
-    ("petal_5.png", 8, (0.5, 1.0, 0.5, 1), 3.0),
-    ("petal_4.png", 8, (0.8, 0.0, 0.0, 1), 3.0),
-    ("petal_6.png", 8, (1.0, 1.0, 0.0, 1), 3.0),
-    ("petal_1.png", 9, (1.0, 0.3, 0.3, 1), 3.0),
-    ("petal_2.png", 8, (0.1, 0.8, 0.1, 1), 3.0),
-    ("petal_3.png", 5, (0.8, 0.0, 0.8, 1), 3.0),
-    ("petal_5.png", 8, (0.5, 1.0, 0.5, 1), 3.0),
-    ("petal_4.png", 8, (0.8, 0.0, 0.0, 1), 3.0),
-    ("petal_6.png", 8, (1.0, 1.0, 0.0, 1), 3.0),
+    ("petal_1.png", 9, (1.0, 0.3, 0.3, 1)),
+    ("petal_2.png", 8, (0.1, 0.8, 0.1, 1)),
+    ("petal_3.png", 5, (0.8, 0.0, 0.8, 1)),
+    ("petal_5.png", 8, (0.5, 1.0, 0.5, 1)),
+    ("petal_4.png", 8, (0.8, 0.0, 0.0, 1)),
+    ("petal_6.png", 8, (1.0, 1.0, 0.0, 1)),
 ]
-level = Level(num_rings, ring_specs)
+course_spec = []
+for i in range(30):
+    ratio = float(i) / 30.0
+    course_spec.append(
+        (
+            math.sin(2 * math.pi * ratio),
+            10.0
+        )
+    )
+level = Level(course_spec, ring_specs)
 
 
 base.run()
